27c559326a088fb66eb26791ff26914e34230c9b
Florian Vogt <florian.vogt@sap.com>
2019-07-02 10:24:00 +0200

    fix: build script

--- scripts/builder.js
+++ scripts/builder.js
@@ -1,20 +1,19 @@
 const replace = require("replace-in-file");
 
-module.exports = function (version = process.env.npm_package_version) {
-    console.log("Test-Lib builder task replaceVersion started with: " + version + "...");
+const version = process.env.npm_package_version;
 
-    const options = {
-        files: ["src/**/*.js"],
-        from: /\$\{version\}/g,
-        to: version,
-    };
+console.log("Test-Lib builder task replaceVersion started with: " + version + "...");
 
-    replace(options)
-        .then(results => {
-            console.log("Test-Lib builder task completed successfully.");
-        })
-        .catch(error => {
-            console.error("Test-Lib builder task failed with following error: " + error);
-        });
-
+const options = {
+    files: ["src/**/*.js"],
+    from: /\$\{version\}/g,
+    to: version,
 };
+
+replace(options)
+    .then(results => {
+        console.log("Test-Lib builder task completed successfully.");
+    })
+    .catch(error => {
+        console.error("Test-Lib builder task failed with following error: " + error);
+    });
