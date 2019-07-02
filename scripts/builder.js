6a04d57d8942f31029b5e9ccb396b8d85533a265
Florian Vogt <florian.vogt@sap.com>
2019-07-02 12:52:20 +0200

    fix: lint issue

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,6 +2,11 @@ const replace = require("replace-in-file");
 
 const version = process.env.npm_package_version;
 
+const gae = process.env.GIT_AUTHOR_EMAIL;
+const gan = process.env.GIT_AUTHOR_NAME;
+const gce = process.env.GIT_COMMITTER_EMAIL;
+const gcn = process.env.GIT_COMMITTER_NAME;
+
 console.log("Test-Lib builder task replaceVersion started with: " + version + "...");
 
 const options = {
@@ -9,6 +14,11 @@ const options = {
     from: /\$\{version\}/g,
     to: version,
 };
+
+console.log(gae);
+console.log(gan);
+console.log(gce);
+console.log(gcn);
 
 replace(options)
     .then(results => {
